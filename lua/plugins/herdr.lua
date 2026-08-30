d13f2b6f5b8918c6a6f5936bea7d49ad9535879c
ttwd <ben.l.backhouse@gmail.com>
2026-08-30 23:45:56 +1000

    Temporary hack so herdr-nvim doesn't setup keys twice

--- lua/plugins/herdr.lua
+++ lua/plugins/herdr.lua
@@ -1,1 +1,13 @@
-return { { "ChmaraX/herdr-nvim", opts = {} } }
+return {
+  {
+    "ChmaraX/herdr-nvim",
+    opts = {},
+    config = function(_, opts)
+      local hn = require("herdr-nvim")
+      hn.setup(opts)
+      -- herdr's sidebar daemon always calls setup() again on VimEnter; disable
+      -- keymap install so that second call does not warn about maps we just set.
+      hn.config.keymaps = false
+    end,
+  },
+}
