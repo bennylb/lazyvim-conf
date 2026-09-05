6b506c8888031f0fcc948c5085127dd4f57e7941
ttwd <ben.l.backhouse@gmail.com>
2026-09-05 21:00:45 +1000

    Recommended herdr-nvim config shape

--- lua/plugins/herdr.lua
+++ lua/plugins/herdr.lua
@@ -1,13 +1,6 @@
 return {
   {
     "ChmaraX/herdr-nvim",
-    opts = {},
-    config = function(_, opts)
-      local hn = require("herdr-nvim")
-      hn.setup(opts)
-      -- herdr's sidebar daemon always calls setup() again on VimEnter; disable
-      -- keymap install so that second call does not warn about maps we just set.
-      hn.config.keymaps = false
-    end,
+    opts = { keymaps = false },
   },
 }
